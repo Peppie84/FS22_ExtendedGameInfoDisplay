f930f5ba3663f54fbac2839c218c7930e876e341
Dennis Schmitt <peppie23@gmail.com>
2023-04-11 18:52:56 +0200

    Prepare for public - relocate some files - added readme file - added prepare-release script

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # FS22_ExtendedGameInfoDisplay
+
+[![FarmingSimulator-22](https://img.shields.io/badge/FarmingSimulator-22-blue?style=plastic)](https://www.farming-simulator.com/)
+[![Modhub Version](https://img.shields.io/badge/Modhub-v1.0.0.0-green?style=plastic)](https://farming-simulator.com/mod.php?mod_id=267536)
+[![GitHub issues](https://img.shields.io/github/issues/Peppie23/FS22_ExtendedGameInfoDisplay?style=plastic)](https://github.com/Peppie23/FS22_ExtendedGameInfoDisplay/issues)
+
+<img src="documents/mod_icon.jpg" style="display: block;margin-left: auto;margin-right: auto; width: 256px;">
+
+Expands the current GameInfoDisplay in the upper right corner by displaying the current year under the date and activates a hidden temperature feature with an indicator of whether the temperature is falling, staying constant or rising, the display of the current temperature and the min/max temperature of the day.
+
+<img src="documents/screen1.png" style="display: block;margin-left: auto;margin-right: auto; width: 50%;">
+
+## Credits
+* Woeller ([Youtube](https://www.youtube.com/@woeller))
+* Sqeep
+
+## Copyright
+Copyright (c) 2023 [Dennis Schmitt](https://github.com/Peppie23).
+All rights reserved.
---
+++ prepare-release.ps1
@@ -0,0 +1,38 @@
+#
+# Use this script to generate a zip file to upload to farmingsimulators modhub
+#
+$modDirectory = "FS22_ExtendedGameInfoDisplay"
+$modName = "$modDirectory";
+$modDescFileName = "$modDirectory/modDesc.xml"
+$testRunnerLogPath = ".\.testrunner\"
+$readmeFilePath = "README.md"
+
+$xmlData = [xml] (Get-Content $modDescFileName -Encoding UTF8)
+$currentVersion = $xmlData.modDesc.version
+
+$newVersion = read-host -Prompt "Please enter a new version (like: a.b.c.d). Current Version is: $currentVersion "
+if (([string]::IsNullOrEmpty($newVersion)))
+{
+    $newVersion = "$currentVersion"
+}
+
+# Write new moddesc version
+$xmlData = [xml] (Get-Content $modDescFileName -Encoding UTF8)
+$xmlData.modDesc.version = "$newVersion"
+
+$utf8WithoutBom = New-Object System.Text.UTF8Encoding($false)
+$xmlStreamWriter = New-Object System.IO.StreamWriter($modDescFileName, $false, $utf8WithoutBom)
+
+$xmlData.Save( $xmlStreamWriter )
+$xmlStreamWriter.Close()
+
+# Write new readme version
+((Get-Content -path $readmeFilePath -Raw) -replace "Modhub-v$currentVersion","Modhub-v$newVersion") | Set-Content -NoNewline -Path $readmeFilePath
+
+
+# Make test run (add testrunner path to your environment path)
+New-Item -ItemType Directory -Force -Path "$testRunnerLogPath"
+TestRunner_public --logPath "$testRunnerLogPath" --outputPath "$testRunnerLogPath" --noPause "$modDirectory\"
+
+# Make zip file
+Compress-Archive -Force -Path "$modDirectory\*" -DestinationPath "$modName.zip"
